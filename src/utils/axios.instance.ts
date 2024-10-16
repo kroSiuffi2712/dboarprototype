394eb7d70ba3c6fe4bc51545a7f3defdf77db08c
cruizs <kroly2@yahoo.es>
2024-10-15 20:25:25 -0500

    update base url by default

--- src/utils/axios.instance.ts
+++ src/utils/axios.instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL ?? "https://jsonplaceholder.typicode.com";
+const API_BASE_URL = "https://jsonplaceholder.typicode.com";
+//process.env.REACT_APP_API_BASE_URL ?? "https://jsonplaceholder.typicode.com";
 
 const headers = {
   "Content-Type": "application/json",
